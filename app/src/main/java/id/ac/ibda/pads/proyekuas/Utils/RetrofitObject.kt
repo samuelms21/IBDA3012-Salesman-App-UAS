bf830c551d8bec63e26da4203844985938e81173
Victor Chendra <victorchendra02@gmail.com>
2023-05-21 15:28:49 +0700

    Update RetrofitObject.kt

--- app/src/main/java/id/ac/ibda/pads/proyekuas/Utils/RetrofitObject.kt
+++ app/src/main/java/id/ac/ibda/pads/proyekuas/Utils/RetrofitObject.kt
@@ -7,9 +7,10 @@ object RetrofitObject {
 
     private const val BASE_URL: String = "http://10.0.2.2:5000/"
 
-    val apiService: ApiService = Retrofit.Builder()
-        .baseUrl(BASE_URL)
-        .addConverterFactory(GsonConverterFactory.create())
-        .build().create(ApiService::class.java)
-
+    val apiService: ApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build().create(ApiService::class.java)
+    }
 }
