56eee34322a5a23d20efce9b1046cf342dd7abbc
Victor Chendra <victorchendra02@gmail.com>
2023-05-22 11:04:51 +0700

    Adding navController

--- app/src/main/java/id/ac/ibda/pads/proyekuas/View/LoginFragment.kt
+++ app/src/main/java/id/ac/ibda/pads/proyekuas/View/LoginFragment.kt
@@ -8,6 +8,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import id.ac.ibda.pads.proyekuas.R
 import id.ac.ibda.pads.proyekuas.ViewModel.LoginVM
 import id.ac.ibda.pads.proyekuas.databinding.FragmentLoginBinding
@@ -18,16 +20,18 @@ class LoginFragment : Fragment() {
 
     private lateinit var binding: FragmentLoginBinding
     private lateinit var loginVM: LoginVM
+    private lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Initialize binding & VM
+        // Initialize binding, VM, & navController
         binding = FragmentLoginBinding.inflate(inflater, container, false)
         loginVM = ViewModelProvider(requireActivity())[LoginVM::class.java]
 
         val view = binding.root
+        navController = view.findNavController()
 
 
         // ######### CODE START HERE #########
@@ -38,6 +42,7 @@ class LoginFragment : Fragment() {
             val username = binding.salesPersonUsernameField.text.toString()
             val password = binding.salesPersonPasswordField.text.toString()
             Log.d("TEST_LOGIN", "Username: ${username}, Password: ${password}")
+//            navController.navigate(R.id.action_loginFragment_to_homepageFragment5)  // navigate to other fragment
         }
 
 //        loginVM.salesperson.observe(viewLifecycleOwner, Observer {
